d25f3e3db057c8873852425baf3cc99e89db6f8d
Piyush Bagad <piyushnbagad11@gmail.com>
2021-10-07 13:52:08 +0200

    Minor

--- run_train.sh
+++ run_train.sh
@@ -18,8 +18,10 @@ logs_dir=$output_dir/logs/
 mkdir -p $logs_dir
 
 # dataset paths
-dataset_dir=/local-ssd/pbagad/datasets/EPIC-KITCHENS-100/EPIC-KITCHENS/
-annotations_dir=/local-ssd/pbagad/datasets/EPIC-KITCHENS-100/annotations/
+# dataset_dir=/local-ssd/pbagad/datasets/EPIC-KITCHENS-100/EPIC-KITCHENS/
+dataset_dir=/var/scratch/pbagad/datasets/EPIC-KITCHENS-100/EPIC-KITCHENS/
+# annotations_dir=/local-ssd/pbagad/datasets/EPIC-KITCHENS-100/annotations/
+annotations_dir=/var/scratch/pbagad/datasets/EPIC-KITCHENS-100/annotations/
 
 # run training
 python tools/run_net.py \
