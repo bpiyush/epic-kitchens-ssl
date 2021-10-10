bea09aaca468f24482ecc1cc64223028e7d8956e
Piyush Bagad <piyushnbagad11@gmail.com>
2021-10-10 22:25:02 +0200

    Adds all configs in one place and minor fixes in wandb

--- tools/log_to_wandb.py
+++ tools/log_to_wandb.py
@@ -1,5 +1,5 @@
 """Script to upload old runs to W&B via log file."""
-from os.path import join, exists, basename, splitext
+from os.path import join, exists, basename, splitext, expanduser
 from tqdm import tqdm
 import wandb
 import pandas as pd
@@ -25,7 +25,7 @@ def get_logged_stats(lines):
     return stat_lines
 
 
-def init_wandb(name, project="video-ssl", entity="uva-vislab", dir="/var/scratch/pbagad"):
+def init_wandb(name, project="video-ssl", entity="uva-vislab", dir=expanduser("~")):
     wandb.init(name=name, project=project, entity=entity, dir=dir)
 
 
@@ -36,33 +36,38 @@ def get_iter_count(ep, it):
 
 
 if __name__ == "__main__":
-    # log_path = "/home/pbagad/expts/epic-kitchens-ssl/R2PLUS1D_8x8_R2+1D_K400/logs/train_logs.txt"
-    # log_path = "/home/pbagad/expts/epic-kitchens-ssl/SLOWFAST_8x8_R50_k400-pretrain/logs/train_logs.txt"
-    # log_path = "/home/pbagad/expts/epic-kitchens-ssl/CTP_8x8_R2Plus1D_k400/logs/train_logs_1.txt"
 
     # SlowFast
-    # log_path = "/var/scratch/pbagad/expts/epic-kitchens-ssl/SLOWFAST_8x8_R50_k400-pretrain/logs/train_logs.txt"
-    # cfg_path = "/var/scratch/pbagad/projects/epic-kitchens-ssl/configs/EPIC-KITCHENS/SLOWFAST_8x8_R50_k400-pretrain.yaml"
+    # cfg_path = "/home/pbagad/projects/epic-kitchens-ssl/configs/EPIC-KITCHENS/SLOWFAST/32x224x224_R50_K400_8x8.yaml"
+    # log_path = "/home/pbagad/expts/epic-kitchens-ssl/32x224x224_R50_K400_8x8/logs/train_logs.txt"
 
     # CTP Model
-    # log_path = "/var/scratch/pbagad/projects/epic-kitchens-slowfast/logs/ctp_on_epic_train.txt"
-    # cfg_path = "/var/scratch/pbagad/projects/epic-kitchens-slowfast/configs/EPIC-KITCHENS/CTP_8x8_R2Plus1D_k400.yaml"
+    cfg_path = "/home/pbagad/projects/epic-kitchens-ssl/configs/EPIC-KITCHENS/CTP/32x112x112_R2+1D-18_K400_LR0.01.yaml"
+    log_path = "/home/pbagad/expts/epic-kitchens-ssl/32x112x112_R2+1D-18_K400_LR0.01/logs/train_logs.txt"
 
-    # R2+1D with original hyperparameters
-    # log_path = "/var/scratch/pbagad/projects/epic-kitchens-slowfast/logs/r2+1d_on_epic_train_from_k400-pretrain.txt"
-    # cfg_path = "/var/scratch/pbagad/projects/epic-kitchens-slowfast/configs/EPIC-KITCHENS/R2PLUS1D_8x8_R2+1D_K400.yaml"
+    # R2+1D with original hyperparameters (LR = 0.01)
+    # cfg_path = "/home/pbagad/projects/epic-kitchens-ssl/configs/EPIC-KITCHENS/R2PLUS1D/32x112x112_R18_K400_LR0.01.yaml"
+    # log_path = "/home/pbagad/expts/epic-kitchens-ssl/32x112x112_R18_K400_LR0.01/logs/train_logs.txt"
 
     # R2+1D with LR = 0.0025
-    # log_path = "/home/pbagad/expts/epic-kitchens-ssl/32x112x112_R18_K400_LR0.0025/logs/train_logs.txt"
     # cfg_path = "/home/pbagad/projects/epic-kitchens-ssl/configs/EPIC-KITCHENS/R2PLUS1D/32x112x112_R18_K400_LR0.0025.yaml"
+    # log_path = "/home/pbagad/expts/epic-kitchens-ssl/32x112x112_R18_K400_LR0.0025/logs/train_logs.txt"
     
     # R2+1D with LR = 1e-4
-    log_path = "/home/pbagad/expts/epic-kitchens-ssl/32x112x112_R18_K400_LR0.0001/logs/train_logs.txt"
-    cfg_path = "/home/pbagad/projects/epic-kitchens-ssl/configs/EPIC-KITCHENS/R2PLUS1D/32x112x112_R18_K400_LR0.0025.yaml"
+    # cfg_path = "/home/pbagad/projects/epic-kitchens-ssl/configs/EPIC-KITCHENS/R2PLUS1D/32x112x112_R18_K400_LR0.0001.yaml"
+    # log_path = "/home/pbagad/expts/epic-kitchens-ssl/32x112x112_R18_K400_LR0.0001/logs/train_logs.txt"
+
+    # R2+1D with LR = 1e-3
+    # cfg_path = "/home/pbagad/projects/epic-kitchens-ssl/configs/EPIC-KITCHENS/R2PLUS1D/32x112x112_R18_K400_LR0.001.yaml"
+    # log_path = "/home/pbagad/expts/epic-kitchens-ssl/32x112x112_R18_K400_LR0.001/logs/train_logs.txt"
 
     # R2+1D with LR 0.0025 and consecutive 32 frames
-    # log_path = "/var/scratch/pbagad/projects/epic-kitchens-slowfast/logs/consecutive_32x112x112_R18_K400_LR0.0025.txt"
-    # cfg_path = "/var/scratch/pbagad/projects/epic-kitchens-slowfast/configs/EPIC-KITCHENS/R2PLUS1D/consecutive_32x112x112_R18_K400_LR0.0025.yaml"
+    # cfg_path = "/home/pbagad/projects/epic-kitchens-ssl/configs/EPIC-KITCHENS/R2PLUS1D/32x112x112_SR_1_R18_K400_LR0.0025.yaml"
+    # log_path = "/home/pbagad/expts/epic-kitchens-ssl/32x112x112_SR_1_R18_K400_LR0.0025/logs/train_logs.txt"
+
+    # R2+1D with LR 0.0025 and temporal resolution 4 (32 out of 128 frames are chosen in a clip)
+    # cfg_path = "/home/pbagad/projects/epic-kitchens-ssl/configs/EPIC-KITCHENS/R2PLUS1D/32x112x112_SR_4_R18_K400_LR0.0025.yaml"
+    # log_path = "/home/pbagad/expts/epic-kitchens-ssl/32x112x112_SR_4_R18_K400_LR0.0025/logs/train_logs.txt"
 
     # load cfg
     args = parse_args()
@@ -99,7 +104,9 @@ if __name__ == "__main__":
     metrics = [x for x in df.columns if "_acc" in x]
 
     # initialize W&B
-    init_wandb(name=splitext(basename(cfg_path))[0], entity="uva-vislab", project="video-ssl")
+    run_name = cfg_path.split("epic-kitchens-ssl/configs/")[-1]
+    run_name = run_name.replace("/", " | ")
+    init_wandb(name=run_name, entity="uva-vislab", project="video-ssl")
 
     # upload config
     config = wandb.config
@@ -111,7 +118,7 @@ if __name__ == "__main__":
     train_df = train_df.rename(columns={k: "train/" + k for k in (metrics + losses + ["epoch", "step"])})
     train_records = train_df.to_dict('records')
     for r in tqdm(train_records, desc="Logging train stats"):
-        wandb.log(r, commit=False)
+        wandb.log(r)
 
     # log val stats
     val_df = df[df["_type"].apply(lambda x: "val" in x)]
