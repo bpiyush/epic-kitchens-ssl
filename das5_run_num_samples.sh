22ff573831185a45fb055b0dc3a30fa2d78a9bdc
Piyush Bagad <piyushnbagad11@gmail.com>
2022-03-22 14:15:06 +0100

    Minor

--- das5_run_num_samples.sh
+++ das5_run_num_samples.sh
@@ -1,8 +1,9 @@
 # Runs ablation on number of training samples
 
 model="R2PLUS1D"
-base_cfg_name="32x112x112_R18_K400_LR0.0025.yaml"
-for num in {2000,4000,8000,16000,32000}
+# base_cfg_name="32x112x112_R18_K400_LR0.0025.yaml"
+base_cfg_name="32x112x112_R18_scratch_LR0.0025.yaml"
+for num in {1000,2000,4000,8000,16000,32000}
 do
     echo "-------------------------- Running $model with $num samples --------------------"
     cfg="configs/EPIC-KITCHENS/$model/n_samples_"$num"_"$base_cfg_name
