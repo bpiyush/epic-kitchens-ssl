7877bb93227da704c828b6e8f905edbbfbf16049
Piyush Bagad <piyushnbagad11@gmail.com>
2022-03-23 11:04:57 +0100

    Adds GDT configs for dataset size ablation

--- das5_run_num_samples.sh
+++ das5_run_num_samples.sh
@@ -1,12 +1,14 @@
 # Runs ablation on number of training samples
 
-model="R2PLUS1D"
+# model="R2PLUS1D"
 # base_cfg_name="32x112x112_R18_K400_LR0.0025.yaml"
-base_cfg_name="32x112x112_R18_scratch_LR0.0025.yaml"
+model="GDT"
+base_cfg_name="32x112x112_R2+1D_K400_LR0.0025.yaml"
 for num in {1000,2000,4000,8000,16000,32000}
 do
     echo "-------------------------- Running $model with $num samples --------------------"
-    cfg="configs/EPIC-KITCHENS/$model/n_samples_"$num"_"$base_cfg_name
+    # cfg="configs/EPIC-KITCHENS/$model/n_samples_"$num"_"$base_cfg_name
+    cfg="configs/EPIC-KITCHENS/$model/das5_n_samples_"$num"_"$base_cfg_name
 
     echo "Config: "
     ls $cfg
