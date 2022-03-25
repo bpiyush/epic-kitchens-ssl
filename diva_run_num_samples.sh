15a5a198bad2e6e45a562dd0728b03a3e9f0776a
Piyush Bagad <piyushnbagad11@gmail.com>
2022-03-25 10:32:49 +0100

    Adds MoCo configs for dataset size ablation

--- diva_run_num_samples.sh
+++ diva_run_num_samples.sh
@@ -4,8 +4,10 @@
 # model="AVID_CMA"
 # model="CTP"
 # base_cfg_name="32x112x112_R2+1D-18_K400_LR0.0025.yaml"
-model="RSPNET"
-base_cfg_name="32x112x112_R18_K400_LR0.0025.yaml"
+# model="RSPNET"
+# base_cfg_name="32x112x112_R18_K400_LR0.0025.yaml"
+model="MOCO"
+base_cfg_name="32x112x112_R18_K400_LR0.00025.yaml"
 for num in {1000,2000,4000,8000,16000,32000}
 do
     echo "-------------------------- Running $model with $num samples --------------------"
