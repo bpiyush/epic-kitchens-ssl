21a3812a1652974343bc4c1a8eb56125f981323f
Piyush Bagad <piyushnbagad11@gmail.com>
2022-03-31 11:15:07 +0200

    Minor

--- diva_run_num_samples.sh
+++ diva_run_num_samples.sh
@@ -6,8 +6,10 @@
 # base_cfg_name="32x112x112_R2+1D-18_K400_LR0.0025.yaml"
 # model="RSPNET"
 # base_cfg_name="32x112x112_R18_K400_LR0.0025.yaml"
-model="MOCO"
-base_cfg_name="32x112x112_R18_K400_LR0.00025.yaml"
+# model="MOCO"
+# base_cfg_name="32x112x112_R18_K400_LR0.00025.yaml"
+model="TCLR"
+base_cfg_name="32x112x112_R18_K400_LR0.0025_no_norm.yaml"
 for num in {1000,2000,4000,8000,16000,32000}
 do
     echo "-------------------------- Running $model with $num samples --------------------"
