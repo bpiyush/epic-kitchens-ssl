0bb5ad26e499cd5eb571999a73ec3c1118d70a8d
Piyush Bagad <piyushnbagad11@gmail.com>
2022-03-31 11:59:49 +0200

    Minor

--- das5_run_num_samples.sh
+++ das5_run_num_samples.sh
@@ -6,8 +6,10 @@
 # base_cfg_name="32x112x112_R2+1D_K400_LR0.0025.yaml"
 # model="PRETEXT_CONTRAST"
 # base_cfg_name="32x112x112_PC_R18_K400_LR0.0025.yaml"
-model="SELAVI"
-base_cfg_name="32x112x112_R18_K400_LR0.0025.yaml"
+# model="SELAVI"
+# base_cfg_name="32x112x112_R18_K400_LR0.0025.yaml"
+model="TCLR"
+base_cfg_name="32x112x112_R18_K400_LR0.0025_no_norm.yaml"
 for num in {1000,2000,4000,8000,16000,32000}
 do
     echo "-------------------------- Running $model with $num samples --------------------"
@@ -18,8 +20,8 @@ do
     ls $cfg
     echo ""
 
-    bash das5_train.sh -c $cfg
-    bash das5_val.sh -c $cfg
+    # bash das5_train.sh -c $cfg
+    # bash das5_val.sh -c $cfg
 
     echo "-------------------------- xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx --------------------"
     echo ""
